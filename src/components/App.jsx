7bc37ac61fd44d2c7e22aa0aabedfe077d207186
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-26 15:41:52 +0300

    Header jsx css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Header from './Header/Header';
+//figpea.com/ файл макету у ../images
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Header />
     </div>
   );
 };
